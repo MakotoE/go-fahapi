1ccba994778604a5def0555102e06036ca67669a
Makoto <36318069+MakotoE@users.noreply.github.com>
2020-04-25 18:03:55 -0700

    QueueInfo() - Return []SlotQueueInfo (#11)

--- types.go
+++ types.go
@@ -3,6 +3,8 @@ package fahapi
 import (
 	"github.com/pkg/errors"
 	"strconv"
+	"strings"
+	"time"
 )
 
 type Options struct {
@@ -371,4 +373,158 @@ func NewPower(s string) (Power, error) {
 
 func isTrue(s string) bool {
 	return s == "true"
+}
+
+type slotQueueInfoRaw struct {
+	ID             string `json:"id"`
+	State          string `json:"state"`
+	Error          string `json:"error"`
+	Project        int    `json:"project"`
+	Run            int    `json:"run"`
+	Clone          int    `json:"clone"`
+	Gen            int    `json:"gen"`
+	Core           string `json:"core"`
+	Unit           string `json:"unit"`
+	PercentDone    string `json:"percentdone"`
+	ETA            string `json:"eta"`
+	PPD            string `json:"ppd"`
+	CreditEstimate string `json:"creditestimate"`
+	WaitingOn      string `json:"waitingon"`
+	NextAttempt    string `json:"nextattempt"`
+	TimeRemaining  string `json:"timeremaining"`
+	TotalFrames    int    `json:"totalframes"`
+	FramesDone     int    `json:"framesdone"`
+	Assigned       string `json:"assigned"`
+	Timeout        string `json:"timeout"`
+	Deadline       string `json:"deadline"`
+	WS             string `json:"ws"`
+	CS             string `json:"cs"`
+	Attempts       int    `json:"attempts"`
+	Slot           string `json:"slot"`
+	TPF            string `json:"tpf"`
+	BaseCredit     string `json:"basecredit"`
+}
+
+type SlotQueueInfo struct {
+	ID             string
+	State          string // TODO create enum type (need official documentation to know every possible value)
+	Error          string // This should also be enum
+	Project        int
+	Run            int
+	Clone          int
+	Gen            int
+	Core           string
+	Unit           string
+	PercentDone    string
+	ETA            time.Duration
+	PPD            int
+	CreditEstimate int
+	WaitingOn      string
+	NextAttempt    time.Duration
+	TimeRemaining  time.Duration
+	TotalFrames    int
+	FramesDone     int
+	Assigned       time.Time
+	Timeout        time.Time
+	Deadline       time.Time
+	WS             string // This could be an IP address but I don't know for sure
+	CS             string
+	Attempts       int
+	Slot           string
+	TPF            time.Duration
+	BaseCredit     int
+}
+
+func (s *SlotQueueInfo) fromRaw(r *slotQueueInfoRaw) error {
+	var err error
+	s.ID = r.ID
+	s.State = r.State
+	s.Error = r.Error
+	s.Project = r.Project
+	s.Run = r.Run
+	s.Clone = r.Clone
+	s.Gen = r.Gen
+	s.Core = r.Core
+	s.Unit = r.Unit
+	s.PercentDone = r.PercentDone
+	s.ETA, err = parseFAHDuration(r.ETA)
+	if err != nil {
+		return err
+	}
+	s.PPD, err = strconv.Atoi(r.PPD)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.CreditEstimate, err = strconv.Atoi(r.CreditEstimate)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.WaitingOn = r.WaitingOn
+	s.NextAttempt, err = parseFAHDuration(r.NextAttempt)
+	if err != nil {
+		return err
+	}
+	s.TotalFrames = r.TotalFrames
+	s.FramesDone = r.FramesDone
+	s.Assigned, err = time.Parse(time.RFC3339, r.Assigned)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.Timeout, err = time.Parse(time.RFC3339, r.Timeout)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.Deadline, err = time.Parse(time.RFC3339, r.Deadline)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	s.WS = r.WS
+	s.CS = r.CS
+	s.Attempts = r.Attempts
+	s.Slot = r.Slot
+	s.TPF, err = parseFAHDuration(r.TPF)
+	if err != nil {
+		return err
+	}
+	s.BaseCredit, err = strconv.Atoi(r.BaseCredit)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+var parseFAHDurationReplacer = strings.NewReplacer(
+	" ", "",
+	"days", "d",
+	"day", "d",
+	"hours", "h",
+	"hour", "h",
+	"mins", "m",
+	"min", "m",
+	"secs", "s",
+	"sec", "s",
+)
+
+func parseFAHDuration(s string) (time.Duration, error) {
+	shortened := parseFAHDurationReplacer.Replace(s)
+	dIndex := strings.IndexByte(shortened, 'd')
+	days := 0.0
+	if dIndex > -1 {
+		daysTemp, err := strconv.ParseFloat(shortened[:dIndex], 64)
+		if err != nil {
+			return 0, errors.WithStack(err)
+		}
+		days = daysTemp
+
+		if dIndex >= len(shortened)-1 { // s only contains days
+			return time.Hour * 24 * time.Duration(days), nil
+		}
+	}
+
+	duration, err := time.ParseDuration(shortened[dIndex+1:])
+	if err != nil {
+		return 0, errors.WithStack(err)
+	}
+
+	return duration + time.Hour*24*time.Duration(days), nil
 }
